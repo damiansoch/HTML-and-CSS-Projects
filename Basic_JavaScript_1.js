ceafb851bf1712ee671bfd4ea0ce9a5023decc2b
damiansoch <66294165+damiansoch@users.noreply.github.com>
2022-04-24 07:49:56 +0100

    First commit

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+alert("Hello, World!");
+
+function EmptyTrash(){
+	var EmptyTrashImg = "recycling_recyclebin_empty_delete_trash_1771.png";
+	document.getElementById("trash").src=EmptyTrashImg;
+	}
+	
