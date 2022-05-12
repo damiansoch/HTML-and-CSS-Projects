9f35790537ef0747b5893ab4b8b7a45da58c710c
damiansoch <66294165+damiansoch@users.noreply.github.com>
2022-05-12 11:31:55 +0100

    portfilio website completed

--- Basic_JavaScript_1.js
+++
@@ -1,7 +0,0 @@
-alert("Hello, World!");
-
-function EmptyTrash(){
-	var EmptyTrashImg = "recycling_recyclebin_empty_delete_trash_1771.png";
-	document.getElementById("trash").src=EmptyTrashImg;
-	}
-	
